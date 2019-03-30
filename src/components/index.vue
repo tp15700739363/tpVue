5aa65f897f9c68793862ae868b1ed7e1250466c4
tangpan <332376580@qq.com>
2019-03-30 16:38:49 +0800

    侧边菜单栏

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,71 @@
 <template>
-    <div>
-        <h2>首页</h2>
-    </div>
+  <!-- 整体容器 -->
+  <el-container class="index-container">
+    <!-- 头部 -->
+    <el-header class="my-header">
+      <el-row>
+        <el-col :span="4">
+          <img src="../assets/logo.png" alt>
+        </el-col>
+        <el-col :span="18">
+          <h2>抠脚瞎后台管理系统</h2>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="warning">退出</el-button>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container class="my-container">
+      <!-- 侧边栏 -->
+      <el-aside class="my-aside" width="200px">
+          <!-- 使用饿了吗ui的导航栏菜单实现 -->
+        <el-menu default-active="2" class="el-menu-vertical-demo">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1"><span class="el-icon-menu"></span>选项1</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 主体区域 -->
+      <el-main class="my-main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    name: 'index',
-}
+  name: "index"
+};
 </script>
 
-<style>
-
+<style lang='scss'>
+.index-container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .my-header {
+    height: 60px;
+    background: #b3c0d1;
+    line-height: 60px;
+    h2 {
+      margin: 0;
+      padding: 0;
+      text-align: center;
+    }
+  }
+  .my-container {
+    flex: 1;
+    .my-aside {
+      background: skyblue;
+    }
+    .my-main {
+      background-color: green;
+    }
+  }
+}
 </style>
