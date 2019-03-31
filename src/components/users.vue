b170404e3ee797365d1dbe1f28626c0e97a0f450
tangpan <332376580@qq.com>
2019-03-31 16:17:09 +0800

    下午第一次提交

--- src/components/users.vue
+++ src/components/users.vue
@@ -6,11 +6,11 @@
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
-    <!-- 输入框 -->
+    <!--输入框 -->
     <el-row>
       <el-col :span="6">
-        <el-input placeholder="请输入内容" v-model="input5" class="input-with-select">
-          <el-button slot="append" icon="el-icon-search"></el-button>
+        <el-input placeholder="请输入内容" v-model="sendData.query" @keyup.native.enter="search" class="input-with-select">
+          <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
         </el-input>
       </el-col>
       <el-col :span="12">
@@ -18,17 +18,39 @@
       </el-col>
     </el-row>
     <!-- 表格 -->
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
+    <el-table :data="userList" style="width: 100%" border>
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
+      <el-table-column prop="mobile" label="电话"></el-table-column>
+      <el-table-column prop="mg_state" label="用户状态">
+        <!-- scope 是一个名字 -->
+        <template slot-scope="scope">
+          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <!-- scope 是一个名字 -->
+        <template slot-scope="scope">
+          <!-- 我们可以通过scope.$index 获取索引 scope.row获取这一行的数据 -->
+          <el-button
+            type="primary"
+            size="mini"
+            icon="el-icon-edit"
+            @click="handleEdit(scope.$index, scope.row)"
+            plain
+          ></el-button>
+          <el-button type="danger" size="mini" icon="el-icon-delete" plain></el-button>
+          <el-button type="warning" size="mini" icon="el-icon-check" plain></el-button>
+        </template>
+      </el-table-column>
     </el-table>
-    <!-- 分页 -->
+    <!-- 分页器 -->
     <el-pagination
-      :page-sizes="[100, 200, 300, 400]"
-      :page-size="100"
+      :page-sizes="[5, 10, 15, 18]"
+      :page-size="sendData.pagesize"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="400"
+      :total="total"
     ></el-pagination>
   </div>
 </template>
@@ -38,41 +60,52 @@ export default {
   name: "users",
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
+      // 总条数
+      total: 0,
+      // 接口用到的数据
+      sendData: {
+        query: "",
+        pagenum: 1,
+        pagesize: 10
+      },
+      // 用户数组
+      userList: []
     };
+  },
+  methods: {
+    // 编辑按钮的点击事件
+    handleEdit(index, row) {
+      console.log(index); // 索引
+      console.log(row); // 数据
+    },
+    // 搜索用户
+    async search() {
+      let res = await this.$axios.get("users", {
+        // headers: {
+        //   Authorization: window.sessionStorage.getItem("token")
+        // },
+        params: this.sendData
+      });
+      // console.log(res);
+      this.total = res.data.data.total;
+      this.userList = res.data.data.users;
+    }
+  },
+  // 接口调用
+  created() {
+    this.search();
   }
 };
 </script>
 
-<style lang="scss">
+<style lang='scss'>
 .users-container {
   background-color: #e9eef3;
-  .my-bread{
-      background-color: #d3dce6;
-      height: 50px;
-      line-height: 50px;
+  .my-bread {
+    background-color: #d3dce6;
+    height: 50px;
+    line-height: 50px;
+    padding-left: 10px;
   }
-
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,28 +1,58 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-//导入axios
-import axios from 'axios'
-
-//设置基地址
-axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
-Vue.prototype.$axios = axios
-
-//导入全局样式
+// 导入全局样式
 import './assets/base.scss'
 
-//导入饿了吗ui
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+// 导入饿了么ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+// 导入axios
+import axios from 'axios'
+// 设置基地址
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+// 设置axios拦截器
+// 请求拦截器 请求之前统一设置一些内容 比如 token
+axios.interceptors.request.use(
+  function(config) {
+    // Do something before request is sent
+    // 统一设置token
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    return config
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截器 统一处理响应
+axios.interceptors.response.use(
+  function(response) {
+    // Do something with response data
+    console.log('响应拦截')
+    console.log(response)
+    // 统一进行弹框
+    Vue.prototype.$message.success(response.data.meta.msg)
+    return response
+  },
+  function(error) {
+    // Do something with response error
+    return Promise.reject(error)
+  }
+)
+
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
-//导入路由
+// 导入路由
 import router from './router.js'
 
 new Vue({
   render: h => h(App),
-  //挂载到Vue实例上
+  // 挂载到Vue实例上
   router
 }).$mount('#app')
